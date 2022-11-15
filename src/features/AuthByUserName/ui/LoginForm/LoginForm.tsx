4affdb31f4ef59e6a604d1b3545f248ac087a8a4
Ivan-Dobei <dobej.work@gmail.com>
2022-11-15 17:02:35 +0200

    add husky, pre commit hooks, add login slice

--- src/features/AuthByUserName/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUserName/ui/LoginForm/LoginForm.tsx
@@ -1,25 +1,43 @@
+import {memo, useCallback} from 'react';
 import {classNames} from 'shared/lib/classNames/classNames';
 import cls from './LoginForm.module.scss';
 import {Input} from 'shared/ui/Input/Input';
 import {Button, ButtonTheme} from 'shared/ui/Button/Button';
 import {useTranslation} from 'react-i18next';
+import {useDispatch, useSelector} from 'react-redux';
+import {loginActions} from '../../model/slice/loginSlice';
+import {getLoginState} from '../../model/selectors/getLoginState/getLoginState';
 
 interface LoginFormProps {
     className?: string
 }
 
-export const LoginForm = ({className}: LoginFormProps) => {
+export const LoginForm = memo(({className}: LoginFormProps) => {
    const {t} = useTranslation();
+   const dispatch = useDispatch();
+   const state = useSelector(getLoginState);
+
+   const onChangeUsername = useCallback((value: string) => {
+      dispatch(loginActions.setUsername(value));
+   }, [dispatch]);
+
+   const onChangePassword = useCallback((value: string) => {
+      dispatch(loginActions.setPassword(value));
+   }, [dispatch]);
 
    return (
       <div className={classNames(cls.LoginForm, {}, [className])}>
          <Input
             className={cls.input}
             placeholder={t('Enter your username')}
+            onChange={onChangeUsername}
+            value={state.username}
          />
          <Input
             className={cls.input}
             placeholder={t('Enter your password')}
+            onChange={onChangePassword}
+            value={state.password}
          />
          <Button
             className={cls.loginBtn}
@@ -29,4 +47,4 @@ export const LoginForm = ({className}: LoginFormProps) => {
          </Button>
       </div>
    );
-};
+});
--- src/shared/ui/Input/Input.stories.tsx
+++ src/shared/ui/Input/Input.stories.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import {ComponentMeta, ComponentStory} from '@storybook/react';
-import {ThemeDecorator} from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
-import {Theme} from 'app/providers/ThemeProvider';
 import {Input} from './Input';
 
 export default {
@@ -15,8 +13,6 @@ export default {
 const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
 
 export const Primary = Template.bind({});
-Primary.args = {};
-
-export const Dark = Template.bind({});
-Dark.args = {};
-Dark.decorators = [ThemeDecorator(Theme.DARK)];
+Primary.args = {
+   value: 'Text',
+};
